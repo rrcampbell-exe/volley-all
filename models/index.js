b872a791babd08c6cf7b1bc37a91a0392bf23ca2
Olga <o.smolyakova30@gmail.com>
2021-09-01 13:10:54 -0500

    added associations

--- models/index.js
+++ models/index.js
@@ -1,6 +1,30 @@
+// import all models
 const User = require("./User");
 const Team = require("./Team");
 const Game = require("./Game");
 const Admin = require("./Admin");
 const Player = require("./Player");
+const { Model } = require("sequelize/types");
+
+// create associations
+Team.hasMany(Player, {
+    foreignKey: 'team_id',
+    onDelete: 'SET NULL'
+});
+
+Player.belongsTo(Team, {
+    foreignKey: 'team_id',
+    onDelete: 'SET NULL'
+});
+
+Game.hasOne(Team, {
+    foreignKey: 'team_id',
+    onDelete: 'SET NULL'
+});
+
+Team.hasMany(Game, {
+    foreignKey: 'team_id'
+});
+
+Model.exports ={ User, Team, Game, Admin, Player }
 
