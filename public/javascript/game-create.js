bbc476b443e5c106445a4e5027075fb64876d0fd
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-04 08:38:19 -0500

    Change variables to suit object keys

--- public/javascript/game-create.js
+++ public/javascript/game-create.js
@@ -3,17 +3,17 @@ async function gameCreateHandler(event) {
 
   const home_team = document.querySelector("#home-team").value;
   const away_team = document.querySelector("#away-team").value;
-  const game_date = document.querySelector("#game-date").value;
-  const game_time = document.querySelector("#game_time").value;
+  const date = document.querySelector("#game-date").value;
+  const time = document.querySelector("#game-time").value;
 
-  if (home_team && away_team && game_date && game_time) {
+  if (home_team && away_team && date && time) {
     const response = await fetch("/api/games/", {
       method: "post",
       body: JSON.stringify({
         home_team,
         away_team,
-        game_date,
-        game_time
+        date,
+        time
       }),
       headers: { "Content-Type": "application/json"},
     });
