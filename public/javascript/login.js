afb7ef3e89d3314ba59c891cbb65ad080a4115f8
Kait <kweishaar@hawk.iit.edu>
2021-09-04 13:20:20 -0500

    trash

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -46,7 +46,7 @@ async function loginFormHandler(event) {
 
     if (response.ok) {
       alert("response okay!")
-      document.location.replace("/dashboard/");
+      document.location.replace("/dashboard");
     } else {
       alert("bad job!")
       alert(response.statusText);
