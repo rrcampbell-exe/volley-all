e3bef5bb9b7606974ed8b8c42194df9bd91fc658
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-06 05:32:07 -0500

    Complete team delete functionality for teams added after seeding

--- public/javascript/team-delete.js
+++ public/javascript/team-delete.js
@@ -1,39 +1,21 @@
-const teamDeleteButton = document.querySelectorAll(".team-delete-btn");
+const teamDelBtn = document.querySelectorAll(".team-delete-btn");
 
-Array.prototype.forEach.call(teamDeleteButton, function addClickListener(btn) {
-  btn.addEventListener("click", function(event) {
-    event.preventDefault();
+async function teamDeleteFormHandler(event) {
+  event.preventDefault();
 
-    const id = event.target.id
-  
-    const response = fetch(`/api/teams/${id}`, {
-      method: "DELETE",
-    });
-  
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  })
-})
+  const id = event.target.id
 
-// async function teamDeleteFormHandler(event) {
-//   event.preventDefault();
+  const response = await fetch(`/api/teams/${id}`, {
+    method: "DELETE",
+  });
 
-//   const id = event.target.id
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+}
 
-//   const response = await fetch(`/api/teams/${id}`, {
-//     method: "DELETE",
-//   });
-
-//   if (response.ok) {
-//     document.location.replace("/");
-//   } else {
-//     alert(response.statusText);
-//   }
-// }
-
-// document
-//   .querySelectorAll(".team-delete-btn")
-//   .addEventListener("click", teamDeleteFormHandler);
+for (let i = 0; i < teamDelBtn.length; i++) {
+  teamDelBtn[i].addEventListener("click", teamDeleteFormHandler);
+}
