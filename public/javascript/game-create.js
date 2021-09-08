62347e95e3a67e03525fafcab45430b3bf227571
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-08 17:23:59 -0500

    Update styles

--- public/javascript/game-create.js
+++ public/javascript/game-create.js
@@ -19,7 +19,7 @@ async function gameCreateHandler(event) {
     });
 
     if (response.ok) {
-      window.location.replace("/");
+      window.location.replace("/#schedule");
     } else {
       alert(response.statusText);
     }
